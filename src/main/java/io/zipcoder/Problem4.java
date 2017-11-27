a20c7b44bd355fd46d5d7d4bab755e62999d4bbd
jeffrey <jeffrey@zipcoders-MacBook-Pro-3.local>
2017-11-27 16:29:49 -0500

    JLawrence Problem4

--- src/main/java/io/zipcoder/Problem4.java
+++ src/main/java/io/zipcoder/Problem4.java
@@ -1,4 +1,27 @@
 package io.zipcoder;
 
 public class Problem4 {
-}
+
+
+   // private String[] userInputString = new String[];
+        static final int Westeros = 256;
+        static boolean dothrakiDefense(String wildFire)
+        {
+            int[] count = new int[Westeros];
+
+            for (int i = 0; i < wildFire.length();  i++)
+                count[wildFire.charAt(i)]++;
+            int odd = 0;
+            for (int i = 0; i < Westeros; i++)
+            {
+                if ((count[i] & 1) != 0)
+                    odd++;
+
+                if (odd > 1)
+                    return false;
+            }
+            return true;
+        }
+
+
+    }
--- src/test/java/io/zipcoder/Problem4Test.java
+++ src/test/java/io/zipcoder/Problem4Test.java
@@ -1,4 +1,35 @@
 package io.zipcoder;
+import org.junit.Test;
+import org.junit.Assert;
+import org.junit.Before;
 
 public class Problem4Test {
+
+    private Problem4 direWolf = new Problem4();
+
+    @Test
+    public void dothrakiDefenseTest() {
+
+        Boolean expected = Boolean.TRUE;
+        Boolean actual = direWolf.dothrakiDefense("aaabbbb");
+        Assert.assertEquals(expected, actual);
+    }
+
+
+    @Test
+    public void dothrakiDefenseFalse() {
+
+        Boolean expected = Boolean.FALSE;
+        Boolean actual = direWolf.dothrakiDefense("cdefghmnopqrstuvw");
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void dothrakiDefenseTestTrue() {
+
+        Boolean expected = Boolean.TRUE;
+        Boolean actual = direWolf.dothrakiDefense("cdcdcdcdeeeef");
+        Assert.assertEquals(expected, actual);
+    }
+
 }
